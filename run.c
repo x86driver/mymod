491b609bd8e728f30eaa1c26c76296d059721ee0
doremi <nothing>
2010-12-24 13:56:24 +0800

    可用 c code 發 intent
    註解掉的那一行是用來啟動 HelloWorld 那隻程式

--- run.c
+++ run.c
@@ -4,32 +4,9 @@
 
 int main()
 {
-        char * const envp[] = {
-                "PATH=/sbin:/system/sbin:/system/bin:/system/xbin",
-		"CLASSPATH=/system/framework/am.jar",
-                NULL,};
-
-        char *argv[] = {
-		"/system/bin/env",
-		"CLASSPATH=/system/framework/am.jar",
-                "/system/bin/app_process",
-		"/system/bin",
-		"com.android.commands.am.Am start -n com.android.camera/com.android.camera.VideoCamera",
-                NULL,};
-
-
-//	char *argv[] = {
-//		"/system/a.sh",
-//		NULL,};
-
-	printf("Before...\n");
-//	int ret = execle(argv[0], argv[0], argv[1], argv[2], argv[3], argv[4], NULL, envp);
-//	if (ret < 0)
-//		perror("execve");
-	printf("Done...\n");
-
 	printf("run lo..\n");
-	system("/system/bin/env CLASSPATH=/system/framework/am.jar /system/bin/app_process /system/bin com.android.commands.am.Am start -n com.android.camera/com.android.camera.VideoCamera");
+//	system("/system/bin/env CLASSPATH=/system/framework/am.jar /system/bin/app_process /system/bin com.android.commands.am.Am start -a android.intent.action.MAIN -c android.intent.category.LAUNCHER -f 0x10200000 -n org.credil.helloworldservice/.HelloWorld");
+	system("/system/bin/env CLASSPATH=/system/framework/am.jar /system/bin/app_process /system/bin com.android.commands.am.Am broadcast -a com.garmin.android.apps.rearcamera.action.REAR_CAMERA_EVENT --ei com.garmin.android.apps.rearcamera.extra.REAR_CAMERA_STATE 0");
 	printf("run end..\n");
 	return 0;
 }
