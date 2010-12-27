df841c13a888e9866f1bc4f418740d861c525d35
doremi <nothing>
2010-12-27 16:49:30 +0800

    這個 run.c (/system/bin/video_inten) 用來根據 argv 來傳 intent

--- run.c
+++ run.c
@@ -1,13 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
-int main()
+int main(int argc, char **argv)
 {
-	printf("run lo..\n");
-//	system("/system/bin/env CLASSPATH=/system/framework/am.jar /system/bin/app_process /system/bin com.android.commands.am.Am start -a android.intent.action.MAIN -c android.intent.category.LAUNCHER -f 0x10200000 -n org.credil.helloworldservice/.HelloWorld");
-	system("/system/bin/env CLASSPATH=/system/framework/am.jar /system/bin/app_process /system/bin com.android.commands.am.Am broadcast -a com.garmin.android.apps.rearcamera.action.REAR_CAMERA_EVENT --ei com.garmin.android.apps.rearcamera.extra.REAR_CAMERA_STATE 0");
-	printf("run end..\n");
+	if (argc < 3) {
+		fprintf(stderr, "Usage: %s [r/d] [0/1]\n", argv[0]);
+		fprintf(stderr, "Example:\n");
+		fprintf(stderr, "    %s r 0 rear camera out\n", argv[0]);
+		fprintf(stderr, "    %s r 1 rear camera in\n", argv[0]);
+		fprintf(stderr, "    %s d 0 driving recorder out\n", argv[0]);
+		fprintf(stderr, "    %s d 1 driving recorder in\n", argv[0]);
+		exit(1);
+	}
+	int device = (argv[1][0] == 'r' ? 0 : 1);
+	int state = atoi(argv[2]);
+	char amcmd[] = "/system/bin/env CLASSPATH=/system/framework/am.jar /system/bin/app_process /system/bin com.android.commands.am.Am broadcast";
+	char *action[2] = {"com.garmin.android.apps.rearcamera.action.REAR_CAMERA_EVENT",
+			"com.garmin.android.apps.drivingrecorder.action.DRIVING_RECORDER_EVENT"};
+	char *extra[2] = {"com.garmin.android.apps.rearcamera.extra.REAR_CAMERA_STATE",
+			"com.garmin.android.apps.drivingrecorder.extra.DRIVING_RECORDER_STATE"};
+	char cmd[512];
+	snprintf(cmd, sizeof(cmd), "%s -a %s --ei %s %d", amcmd, action[device], extra[device], state);
+	system(cmd);
 	return 0;
 }
 
